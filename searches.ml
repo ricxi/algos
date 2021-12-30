a22b689cf4f9508d16f9ccb690337880102b5540
irixoc <irixoc@gmail.com>
2021-12-30 15:57:52 -0500

    created insertion sort with helper function insert

--- searches.ml
+++ searches.ml
@@ -3,4 +3,9 @@ let rec insert (x: int) (xs: int list) : int list =
   | [] -> [x]
   | h::t -> if x < h then x :: xs else h :: (insert x t);;
 
-  
+let insertion_sort (xs: int list) : int list =
+  let rec aux (sorted: int list) (unsorted: int list) : int list =
+    match unsorted with 
+    | [] -> sorted
+    | h::t -> aux (insert h sorted) t
+  in aux [] xs;;
